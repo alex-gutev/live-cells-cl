51238a34b7e4004ca35b27d0ca516e6e44d27f8b
Alexander Gutev <alex.gutev@gmail.com>
2024-04-27 13:29:10 +0200

    Add basic macro definitions.

---
+++ src/macros.lisp
@@ -0,0 +1,26 @@
+(in-package :live-cells)
+
+(defmacro defcell (name expression &environment env)
+  (if (constantp expression env)
+      `(define-mutable-cell% ,name ,expression)
+      `(define-computed-cell% ,name ,expression)))
+
+(defmacro define-mutable-cell% (name expression)
+  (let ((spec (make-instance
+               'mutable-cell-spec
+               :name name
+               :init expression)))
+
+    (generate-cell-definition spec)))
+
+(defmacro define-computed-cell% (name expression)
+  (let ((spec (make-instance
+               'compute-cell-spec
+               :name name
+               :init expression)))
+
+    (generate-cell-definition spec)))
+
+(defmacro live (&body forms)
+  (let ((spec (make-instance 'watch-function-spec :body forms)))
+    (generate-watch-function spec)))
