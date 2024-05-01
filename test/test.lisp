23f339960944fbbcf5956e226c1e9c266f130ffe
Alexander Gutev <alex.gutev@gmail.com>
2024-05-01 21:02:18 +0200

    Add documentation for test utilties.

--- test/test.lisp
+++ test/test.lisp
@@ -1,4 +1,4 @@
-;;; Defines the master test suite for live-cells
+;;;; Defines the master test suite for live-cells
 
 (in-package :live-cells/test)
 
@@ -6,6 +6,8 @@
   :description "Master test suite for live-cells.")
 
 (defun test-live-cells ()
+  "Run all the unit tests"
+
   (run! 'live-cells))
 
 
@@ -13,10 +15,23 @@
 ;;; Utilities
 
 (defstruct test-value-observer
+  "An observer that records the values of a cell
+
+VALUES is an array containing the value of the cell at each call to
+the observer.
+
+STOP is a function of no arguments, which stops the observer when it
+is called."
+
   values
   stop)
 
 (defmacro observe-values (cell)
+  "Create a TEST-VALUE-OBSERVER that records all the values produced by CELL.
+
+A value is recorded in the TEST-VALUE-OBSERVER whenever the value of
+CELL changes."
+
   (with-gensyms (first? cell-values value)
     `(let ((,first? t)
            (,cell-values (make-array 0 :adjustable t :fill-pointer t)))
@@ -32,6 +47,17 @@
                  (setf ,first? nil)))))))
 
 (defmacro with-observed-values (cell (values-sym &optional stop-sym) &body body)
+  "Record the value produced by CELL in a vector.
+
+VALUES-SYM is the symbol naming the variable to which the vector,
+holding the values produced by CELL, is bound. STOP-SYM is the symbol
+naming the function, which, when called, stops the recording of
+values. After this function is called further values produced by CELL
+will not be accumulated in VALUES-SYM.
+
+Both the variable named by VALUES-SYM, and the function named by
+STOP-SYM are made available to the forms in BODY."
+
   (with-gensyms (observer)
     `(let ((,observer (observe-values ,cell)))
        (unwind-protect
